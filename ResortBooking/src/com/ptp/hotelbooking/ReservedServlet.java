fd9f65c86c744ae8db3f41820009d301feb8fbff
Shweta <gandhi.shweta88@gmail.com>
2013-11-13 17:12:48 -0600

    Mail is sent with image, link and other details

--- ResortBooking/src/com/ptp/hotelbooking/ReservedServlet.java
+++ ResortBooking/src/com/ptp/hotelbooking/ReservedServlet.java
@@ -2,11 +2,19 @@ package com.ptp.hotelbooking;
 
 import java.io.IOException;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,25 +22,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.google.gson.Gson;
+
 /**
  * Servlet implementation class ReservedServlet
  */
@@ -53,23 +53,19 @@ public class ReservedServlet extends HttpServlet {
 		//content type
 		response.setContentType("text/html");
 		
-		//ResultSet rs=null;
-		
-		Gson gson=new Gson();
+		//Takes Json String from Request.
 		StringBuilder sb = new StringBuilder();
 		BufferedReader reader = request.getReader();
 		String str;
 		PrintWriter out=response.getWriter();
 		
 		while ((str = reader.readLine()) != null) {
-			//System.out.println(str);
-			sb.append(str);
+				sb.append(str);
 		}
 		
-		//System.out.println(sb.toString());
-		ReservationDetails details = new Gson().fromJson(sb.toString(),
-		ReservationDetails.class);
-		//System.out.println("After Gson");
+		//ReservationDetails is a POJO class and this line help in mapping POJO to JSON
+		ReservationDetails details = new Gson().fromJson(sb.toString(),ReservationDetails.class);
+		
 		Connection conn = null;
 		try {
 			//accessing date values from rootscope
@@ -116,6 +112,7 @@ public class ReservedServlet extends HttpServlet {
 		
 		// Step 3: Execute a SQL SELECT query
 		Statement st = conn.createStatement();
+		
 		//first table updation
 		String query="insert into reservation(startdate,enddate,room_no,cust_name) values('"+startDateWebStr+"','"+endDateWebStr+"','"+details.Room_type+"','"+details.email+"')";
 		st.executeUpdate(query);
@@ -123,113 +120,103 @@ public class ReservedServlet extends HttpServlet {
 		//to get reservation id for future updation or deletion
 		String sql1="select * from reservation where startdate='"+startDateWebStr+"'"+"AND enddate='"+endDateWebStr+"'";
 		 ResultSet rs=st.executeQuery(sql1);
-		 ReservationDetails ResDtl=new ReservationDetails();
+		 
 			if(rs.next())
 			{
-				ResDtl.Reserv_id=rs.getInt("res_id");
-				System.out.println(ResDtl.Reserv_id);
+				details.Reserv_id=rs.getInt("res_id");
+				System.out.println(details.Reserv_id);
 			}
 
 		//second table updation
-		String sql = "insert into roomtable(room_no,type_id,res_id) values ('"+details.Room_type+"','"+details.Room_id+"','"+ResDtl.Reserv_id+"')";
+		String sql = "insert into roomtable(room_no,type_id,res_id) values ('"+details.Room_type+"','"+details.Room_id+"','"+details.Reserv_id+"')";
 		int temp = st.executeUpdate(sql);
 		
 		 System.out.println(temp);
-			
-		
-		
-		//sending the data back to controller to take desire action
-		String json=gson.toJson(ResDtl);
-		out.print(json);
-		//st.close();
-		//out.close();
 		 
-		//part is copied from mailservlet.
-			String[] to={details.email};
-    		String[] cc={details.email};
-    		String[] bcc={details.email};
-    		//This is for google
-
-    		String userName="shwetu09@gmail.com";
-    		String passWord="Consultant13";
-    		String host="smtp.gmail.com";
-    		String port="465";
-    		String starttls="true";
-    		String auth="true";
-    	//	boolean debug=true;
-    		String socketFactoryClass="javax.net.ssl.SSLSocketFactory";
-    		String fallback="false";
-    		String subject="Confirmation";
-    		String text="Email id:" +details.email+"\nReservation id"+ResDtl.Reserv_id+"\ncheck in:"+startDateWebStr+"\nCheck Out:"+endDateWebStr;
-
-    		Properties props = new Properties();
-    		//Properties props=System.getProperties();
-    		props.put("mail.smtp.user", userName);
-    		props.put("mail.smtp.host", host);
-    		if(!"".equals(port))
-    			props.put("mail.smtp.port", port);
-    		if(!"".equals(starttls))
-    			props.put("mail.smtp.starttls.enable",starttls);
-    		props.put("mail.smtp.auth", auth);
-    		//changed value from true to false to stop debugging
-    		/*if(debug)
-    		{
-    			props.put("mail.smtp.debug", "false");
-    		}
-    		else
-    		{
-    			props.put("mail.smtp.debug", "false");         
-    		}*/
-    		if(!"".equals(port))
-    			props.put("mail.smtp.socketFactory.port", port);
-    		if(!"".equals(socketFactoryClass))
-    			props.put("mail.smtp.socketFactory.class",socketFactoryClass);
-    		if(!"".equals(fallback))
-    			props.put("mail.smtp.socketFactory.fallback", fallback);
-
-    		try
-    		{
-    			Session session = Session.getDefaultInstance(props, null);
-    			//session.setDebug(debug);
-    			/*MimeMessage msg = new MimeMessage(session);
-    			msg.setText(text);
-    			msg.setSubject(subject);
-    			msg.setFrom(new InternetAddress("shwetu09@gmail.com"));
-    			for(int i=0;i<to.length;i++)
-    			{
-    				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to[i]));
-    			}
-    			for(int i=0;i<cc.length;i++)
-    			{
-    				msg.addRecipient(Message.RecipientType.CC, new InternetAddress(cc[i]));
-    			}
-    			for(int i=0;i<bcc.length;i++)
-    			{
-    				msg.addRecipient(Message.RecipientType.BCC, new InternetAddress(bcc[i]));
-    			}
-    			msg.saveChanges();*/
-    			Transport transport = session.getTransport("smtp");
-    			transport.connect(host, userName, passWord);
-    			//transport.sendMessage(msg, msg.getAllRecipients());
-    			transport.close();
-  			 
-    		}
-    		catch (Exception mex)
-    		{
-    			mex.printStackTrace();
-
-    		
-    		}
-
-    		Mail.sendMail("shwetu09@gmail.com","Consultant13","smtp.gmail.com","465","true","true",true,
-    				"javax.net.ssl.SSLSocketFactory","false",to,cc,bcc,
-    				"hi, Test Mail",
-    		"Finally Your reservation is done"+text);
+		//this is my methodname which I used for sending mail
+		 sendConfirmationEmail(details);
+		 
+		 Gson gson = new Gson();
+			String json=gson.toJson(details);
+			out.print(json);
+			
+		//sending the data back to controller to take desire action
+		
+			 
 		}//closing for try
 		catch (Exception E) {
 			System.out.println("The error is=" + E.getMessage());
 			E.printStackTrace();
 		}
-	}
+		
+	}//closing for post
+	
+	//If above part works it will send mail confirmation.
+		private void sendConfirmationEmail(ReservationDetails returnInfo) {
+			final String username = "shwetu09@gmail.com";
+			final String password = "Consultant13";
+			
+				
+			Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
+			props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+	 
+			Session session = Session.getInstance(props,
+			  new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			  });
+	 
+			try {
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress("shwetu09@gmail.com"));
+				message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(returnInfo.email));
+				
+				message.setSubject("Utopia~way of living reservation confirmation");
+				
+				String htmlText;
+				//I used htmlText instead of message.setText coz I wanted to send image. Not sure if that is right way but, it worked for now.
+				//adds check in, check out, room type, image and website link. 
+				htmlText ="hello Your Check In date is"+ returnInfo.CheckIn+"\n<br>"+"Check Out is" +returnInfo.CheckOut+"\n<br>"+"Room Tyep is"+returnInfo.Room_type+"\n<br>"+"Visit us at http://localhost:9998/ResortBooking/ for resrvation or just find out deals !";
+				htmlText+="<p align=center><img src=\"cid:image\"> </p>";
+				
+				//-----Message with attachments------------------------------
+				MimeMultipart multipart = new MimeMultipart("related");
 
-}
+		        // first part  (the html)
+		     BodyPart messageBodyPart = new MimeBodyPart();
+		     messageBodyPart.setContent(htmlText,"text/html");
+
+		        // add it
+		        multipart.addBodyPart(messageBodyPart);
+		        
+		        // second part (the image)
+		        messageBodyPart = new MimeBodyPart();
+		        DataSource fds = new FileDataSource
+		          ("C:\\pic-hotel\\Logo.png");
+		        messageBodyPart.setDataHandler(new DataHandler(fds));
+		        messageBodyPart.addHeader("Content-ID","<image>");
+		        
+		        // add it
+		        multipart.addBodyPart(messageBodyPart);
+
+		        // put everything together
+		         message.setContent(multipart );
+				Transport.send(message);
+	 
+				System.out.println("Done");
+	 
+			}//end try 
+			catch (MessagingException e) {
+				throw new RuntimeException(e);
+			}//end catch
+		}//end sendconfirmation method
+
+	}//end class
+	
+
